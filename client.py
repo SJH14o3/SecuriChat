4f2dbbb6c389a621d560df94bb26e3031feba1e5
SJH14o3 <sajjadjh1403@gmail.com>
2025-06-03 10:44:38 +0330

    a basic ui for logging in, logic isn't implemented yet

--- client.py
+++ client.py
@@ -4,16 +4,21 @@ import random
 import time
 import json
 import sys
+
+from PySide6.QtCore import QRegularExpression, QTimer
+from PySide6.QtGui import QRegularExpressionValidator
+
 from statics import *
 from server import PORT
 from user import User
 from log import Log
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QMessageBox
 
 isRunning = True # useful to finish threads
 ip_address = None # ip address of incoming messages socket for this client
 port: int = 0 # port of incoming messages socket for this client
 log: Log # log file, it will be initialized after a successful login/sign in. name is socket of incoming messages
+window: QWidget # window of UI
 
 
 def log_in(conn: socket, addr):
@@ -90,7 +95,7 @@ def connect_to_server():
     except ConnectionRefusedError:
         print("Connection refused: make sure server is running.")
     except socket.gaierror as e:
-        print("Invalid address: Could not resolve hostname.")
+        print(f"Invalid address: {e}")
     except TimeoutError:
         print("Connection timed out: Server took too long to respond.")
     except socket.error as e:
@@ -119,31 +124,91 @@ def receive_connection(conn, conn_port: int, conn_addr):
         thread = threading.Thread(target=handle_income_connection, args=(transmitter, conn))
         thread.start()
 
-# a temporary UI just for shutting down client
+# UI class for logging in scene
 class UI(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("SecuriChat")
+        self.setFixedSize(300, 200)
+        self.connected = False
+        self.error_message = None
+        self.connect_to_server()
+        # if connection to server was successful, then we will show the login scene
+        if self.connected:
+            self.switch_button = QPushButton("Switch to Sign Up")
+            self.login_button = QPushButton("Log In")
+            self.password_input = QLineEdit()
+            self.password_label = QLabel("Password:")
+            self.username_input = QLineEdit()
+            self.username_label = QLabel("Username:")
+            self.setup_ui()
+            self.show()
+        else:
+            # if it wasn't then an error is shown and program will exit
+            QTimer.singleShot(0, self.show_connection_failed_dialog)
 
-        self.quit_button = QPushButton("Quit")
-        self.quit_button.clicked.connect(self.quit_app)
+    # show the error dialog when connection to server failed
+    def show_connection_failed_dialog(self):
+        QMessageBox.critical(self, "Critical Error", self.error_message)
+        QApplication.quit()
+
+    # first we need to try to connect to the server and make sure it's running
+    def connect_to_server(self):
+        try:
+            socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_connection.bind(('127.0.0.1', 0))
+            socket_connection.connect(('127.0.0.1', PORT))
+            socket_connection.send(CLIENT_CHECK_SERVER_AVAILABILITY.encode())
+            response = socket_connection.recv(1024).decode()
+            if response != SERVER_CONNECT_OK:
+                self.error_message = "Server response was invalid"
+            else:
+                self.connected = True
+        except ConnectionRefusedError:
+            self.error_message = "Server did not respond\nMake sure server is running."
+        except socket.gaierror:
+            self.error_message = "Invalid address: Could not resolve hostname."
+        except TimeoutError:
+            self.error_message = "Connection timed out: Server took too long to respond."
+        except socket.error as e:
+            self.error_message = f"Socket error: {e}."
+        except Exception as e:
+            self.error_message = f"Unknown error: {e}."
+    # we validate user input
+    def validate_inputs(self):
+        # TODO: complete implementation
+        if self.username_input.hasAcceptableInput() and self.password_input.hasAcceptableInput():
+            QMessageBox.information(self, "Success", "Inputs are valid!")
+        else:
+            QMessageBox.warning(self, "Error", "Invalid username or password format.")
+
+    # setting up log in scene
+    def setup_ui(self):
+
+        username_regex = QRegularExpression(r"^[a-zA-Z0-9_.-]{3,20}$")
+        self.username_input.setValidator(QRegularExpressionValidator(username_regex))
+
+        self.password_input.setEchoMode(QLineEdit.Password)
+        password_regex = QRegularExpression(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$")
+        self.password_input.setValidator(QRegularExpressionValidator(password_regex))
+
+        self.login_button.clicked.connect(self.validate_inputs)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.quit_button)
-        self.setLayout(layout)
+        layout.addWidget(self.username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(self.password_label)
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.login_button)
+        layout.addWidget(self.switch_button)
 
-    def quit_app(self):
-        global isRunning
-        isRunning = False
-        log.append_log("exiting program")
-        QApplication.quit()
+        self.setLayout(layout)
+        self.show()
+
 
 def main():
     app = QApplication(sys.argv)
     window = UI()
-    window.show()
-    thread = threading.Thread(target=connect_to_server)
-    thread.start()
     sys.exit(app.exec())
 
 if __name__ == '__main__':
